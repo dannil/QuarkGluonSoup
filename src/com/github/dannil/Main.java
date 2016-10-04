6a66635c91988da3b46fc35d5c81a0d605743af2
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-10-04 20:05:40 +0200

    Added some test code

--- src/com/github/dannil/Main.java
+++ src/com/github/dannil/Main.java
@@ -1,9 +1,13 @@
 package com.github.dannil;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
+		File file = new File("lst.txt");
+		QuarkGluonSoup q = new QuarkGluonSoup(file);
 
+		String s = q.generatePseudoScientificMess(5);
 	}
-
 }
